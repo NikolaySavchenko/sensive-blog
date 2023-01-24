c4dab40544e73d2da17072eda09821183f8ced36
Nikolay Savchenko <7548568@gmail.com>
2023-01-24 21:22:44 +0300

    Fix admin pages load time2

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,13 @@ from blog.models import Post, Tag, Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('post', 'author',)
-    raw_id_fields = ('post',)
+    raw_id_fields = ('post', 'author')
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+    raw_id_fields = ('author', 'tags', 'likes')
+
+
 admin.site.register(Tag)
