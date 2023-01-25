a86098ccf45404d7e9605e6a2a77c7662174523a
Nikolay Savchenko <7548568@gmail.com>
2023-01-25 20:47:45 +0300

    Ordered the attributes in models

--- blog/models.py
+++ blog/models.py
@@ -39,7 +39,6 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -55,16 +54,16 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Теги')
 
+    class Meta:
+        ordering = ['-published_at']
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post_detail', args={'slug': self.slug})
-
-    class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
 
     objects = PostQuerySet.as_manager()
 
@@ -72,19 +71,19 @@ class Post(models.Model):
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
 
-    def __str__(self):
-        return self.title
-
-    def clean(self):
-        self.title = self.title.lower()
-
-    def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
-
     class Meta:
         ordering = ['title']
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
 
     objects = TagQuerySet.as_manager()
 
@@ -103,10 +102,10 @@ class Comment(models.Model):
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
 
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
