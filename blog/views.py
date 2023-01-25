c979239bb468793c18b3c0895b0e4b271b31d237
Nikolay Savchenko <7548568@gmail.com>
2023-01-25 20:51:31 +0300

    Removed unused code

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@ from blog.models import *
 from django.db.models import Count
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
     return {
         'title': post.title,
